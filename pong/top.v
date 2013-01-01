6e824a75e86a5a9179dbb065b7c761ea0974ef9a
Zheng Yan <yzheng624@gmail.com>
2013-01-01 12:39:52 +0800

    Start of something new

---
+++ pong/top.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+module top(
+		input wire clk, reset,
+		input wire [1:0] btn1,
+		input wire [1:0] btn2,
+		input wire ai_switch,
+		output wire hsync, vsync,
+		output wire [2:0] rgb
+    );
+	 
+	 
+
+
+endmodule
